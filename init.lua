9fb6d78cca20613b50a57467e7cfede521db1f9a
Hassan Kamran <152590873+hassan-owr@users.noreply.github.com>
2024-11-26 10:49:56 +0500

    Modified init.lua

--- init.lua
+++ init.lua
@@ -380,6 +380,8 @@ require("lspconfig").pyright.setup({
 	on_attach = function(client, bufnr)
 		local opts = { noremap = true, silent = true }
 		local buf_set_keymap = vim.api.nvim_buf_set_keymap
+		client.server_capabilities.documentFormattingProvider = false -- Disable pyright formatting
+		client.server_capabilities.documentRangeFormattingProvider = false
 		buf_set_keymap(bufnr, "n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
 		buf_set_keymap(bufnr, "n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
 		buf_set_keymap(bufnr, "n", "<leader>rn", "<Cmd>lua vim.lsp.buf.rename()<CR>", opts)
