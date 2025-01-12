a1ae37a1788b0794faabfc706339477d66016807
Hassan Kamran <152590873+hassan-owr@users.noreply.github.com>
2025-01-12 17:58:37 +0500

    Optimized Plugins

--- init.lua
+++ init.lua
@@ -106,21 +106,30 @@ require("lazy").setup({
 			require("mason").setup()
 
 			require("mason-lspconfig").setup({
-				ensure_installed = { "pyright" }, -- Ensure pyright is installed by Mason
+				ensure_installed = {
+					"ruff",
+					"html",
+					"cssls",
+					"ts_ls",
+					"emmet_ls",
+				},
 				automatic_installation = true,
 			})
 
-			-- Auto-install tools like black via Mason
 			require("mason-null-ls").setup({
-				ensure_installed = { "black", "isort", "flake8", "debugpy", "stylua" }, -- Add more tools as needed
+				ensure_installed = {
+					"ruff",
+					"prettier",
+					"stylua",
+				},
 				automatic_installation = true,
 			})
 		end,
 		dependencies = {
-			"williamboman/mason-lspconfig.nvim", -- Bridge Mason with LSPconfig
-			"jay-babu/mason-null-ls.nvim", -- Bridge Mason with null-ls
-			"neovim/nvim-lspconfig", -- LSP configuration
-			"jose-elias-alvarez/null-ls.nvim", -- Configure formatters/linters
+			"williamboman/mason-lspconfig.nvim",
+			"jay-babu/mason-null-ls.nvim",
+			"neovim/nvim-lspconfig",
+			"jose-elias-alvarez/null-ls.nvim",
 		},
 	},
 
@@ -128,7 +137,7 @@ require("lazy").setup({
 	{
 		"github/copilot.vim",
 		config = function()
-			vim.cmd("Copilot enable") -- This enables GitHub Copilot to start automatically
+			vim.cmd("Copilot enable")
 		end,
 	},
 
@@ -159,26 +168,35 @@ require("lazy").setup({
 		dependencies = { "nvim-lua/plenary.nvim" },
 	},
 
-	-- Null-ls setup for code formatting with black
+	-- Null-ls setup
 	{
 		"jose-elias-alvarez/null-ls.nvim",
 		config = function()
 			local null_ls = require("null-ls")
 			null_ls.setup({
-				debug = true, -- Enable debugging
+				debug = true,
 				sources = {
-					null_ls.builtins.formatting.black.with({
-						extra_args = { "--line-length", "78" },
+					-- Ruff for Python
+					null_ls.builtins.formatting.ruff,
+					null_ls.builtins.diagnostics.ruff,
+
+					-- Web development formatting
+					null_ls.builtins.formatting.prettier.with({
+						filetypes = {
+							"html",
+							"css",
+							"javascript",
+							"typescript",
+							"json",
+						},
 					}),
-					null_ls.builtins.formatting.isort,
-					null_ls.builtins.diagnostics.flake8.with({
-						extra_args = { "--ignore", "E203" },
-					}),
+
+					-- Lua formatting
 					null_ls.builtins.formatting.stylua,
 				},
 			})
 		end,
-		dependencies = { "nvim-lua/plenary.nvim" }, -- Ensure plenary is loaded
+		dependencies = { "nvim-lua/plenary.nvim" },
 	},
 
 	{
@@ -189,8 +207,8 @@ require("lazy").setup({
 	-- Indent-blankline setup for version 3
 	{
 		"lukas-reineke/indent-blankline.nvim",
-		main = "ibl", -- Version 3 configuration
-		opts = {}, -- Empty opts table to use default settings
+		main = "ibl",
+		opts = {},
 	},
 
 	-- Autocompletion and snippets setup
@@ -221,14 +239,13 @@ require("lazy").setup({
 					["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
 					["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
 					["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-					["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+					["<C-y>"] = cmp.config.disable,
 					["<C-e>"] = cmp.mapping({
 						i = cmp.mapping.abort(),
 						c = cmp.mapping.close(),
 					}),
 					["<CR>"] = cmp.mapping.confirm({ select = true }),
 
-					-- LuaSnip snippet navigation
 					["<Tab>"] = cmp.mapping(function(fallback)
 						if cmp.visible() then
 							cmp.select_next_item()
@@ -256,7 +273,6 @@ require("lazy").setup({
 				}),
 			})
 
-			-- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
 			cmp.setup.cmdline("/", {
 				mapping = cmp.mapping.preset.cmdline(),
 				sources = {
@@ -264,7 +280,6 @@ require("lazy").setup({
 				},
 			})
 
-			-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 			cmp.setup.cmdline(":", {
 				mapping = cmp.mapping.preset.cmdline(),
 				sources = cmp.config.sources({
@@ -289,7 +304,6 @@ require("lazy").setup({
 				},
 			})
 
-			-- Lazygit integration
 			local Terminal = require("toggleterm.terminal").Terminal
 			local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
 
@@ -302,17 +316,9 @@ require("lazy").setup({
 	},
 })
 
--- Auto-format Python files on save using black
+-- Auto-format on save
 vim.api.nvim_create_autocmd("BufWritePre", {
-	pattern = "*.py",
-	callback = function()
-		vim.lsp.buf.format({ async = false })
-	end,
-})
-
--- Auto-format Lua files on save using stylua
-vim.api.nvim_create_autocmd("BufWritePre", {
-	pattern = "*.lua",
+	pattern = { "*.py", "*.lua", "*.html", "*.css", "*.js", "*.ts", "*.jsx", "*.tsx" },
 	callback = function()
 		vim.lsp.buf.format({ async = false })
 	end,
@@ -320,26 +326,27 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 
 -- Tree-sitter configuration
 require("nvim-treesitter.configs").setup({
-	ensure_installed = "python", -- or other languages you want to support
-	highlight = {
-		enable = true, -- Enable Tree-sitter based syntax highlighting
+	ensure_installed = {
+		"python",
+		"html",
+		"css",
+		"javascript",
+		"typescript",
+		"lua",
 	},
-	indent = {
-		enable = true, -- Enable Tree-sitter based indentation
-	},
-	fold = {
-		enable = true, -- Enable Tree-sitter based folding
-	},
+	highlight = { enable = true },
+	indent = { enable = true },
+	fold = { enable = true },
 })
 
 require("Comment").setup()
 
--- Set foldmethod to expr and use Treesitter's fold expression
+-- Folding settings
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-vim.opt.foldlevel = 99 -- Open all folds by default
+vim.opt.foldlevel = 99
 
--- Keybinding to manually trigger formatting
+-- Keybindings
 vim.api.nvim_set_keymap(
 	"n",
 	"<leader>f",
@@ -353,11 +360,11 @@ vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
 
--- disable netrw at the very start of your init.lua
+-- Disable netrw
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
--- optionally enable 24-bit color
+-- Enable termguicolors
 vim.opt.termguicolors = true
 
 -- NvimTree setup
@@ -375,21 +382,235 @@ require("nvim-tree").setup({
 	},
 })
 
--- Ensure that pyright is set up
-require("lspconfig").pyright.setup({
+-- LSP Setups
+local on_attach = function(client, bufnr)
+	local opts = { noremap = true, silent = true }
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>rn", "<Cmd>lua vim.lsp.buf.rename()<CR>", opts)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>ca", "<Cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+end
+
+-- Ruff LSP setup
+require("lspconfig").ruff.setup({
 	on_attach = function(client, bufnr)
-		local opts = { noremap = true, silent = true }
-		local buf_set_keymap = vim.api.nvim_buf_set_keymap
-		client.server_capabilities.documentFormattingProvider = false -- Disable pyright formatting
+		client.server_capabilities.documentFormattingProvider = false
 		client.server_capabilities.documentRangeFormattingProvider = false
-		buf_set_keymap(bufnr, "n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
-		buf_set_keymap(bufnr, "n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
-		buf_set_keymap(bufnr, "n", "<leader>rn", "<Cmd>lua vim.lsp.buf.rename()<CR>", opts)
-		buf_set_keymap(bufnr, "n", "<leader>ca", "<Cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+		on_attach(client, bufnr)
 	end,
 })
 
+-- Web development LSP setups
+local web_capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+require("lspconfig").html.setup({
+	capabilities = web_capabilities,
+	on_attach = on_attach,
+})
+
+require("lspconfig").cssls.setup({
+	capabilities = web_capabilities,
+	on_attach = on_attach,
+})
+
+require("lspconfig").ts_ls.setup({
+	capabilities = web_capabilities,
+	on_attach = on_attach,
+})
+
+require("lspconfig").emmet_ls.setup({
+	capabilities = web_capabilities,
+	filetypes = {
+		"html",
+		"css",
+		"javascript",
+		"typescript",
+		"javascriptreact",
+		"typescriptreact",
+	},
+})
+
+-- NvimTree toggle
 vim.api.nvim_set_keymap("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
 
 -- Copilot keybinding
 vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+
+-- Additional LSP diagnostic settings
+vim.diagnostic.config({
+	virtual_text = true,
+	signs = true,
+	underline = true,
+	update_in_insert = false,
+	severity_sort = true,
+})
+
+-- Diagnostic signs
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+	local hl = "DiagnosticSign" .. type
+	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+
+-- Python DAP configuration
+local dap = require("dap")
+local dap_python = require("dap-python")
+dap_python.setup("python")
+
+-- Configure Python test methods
+dap_python.test_runner = "pytest"
+
+-- DAP keymappings
+vim.keymap.set("n", "<leader>db", function()
+	require("dap").toggle_breakpoint()
+end)
+vim.keymap.set("n", "<leader>dc", function()
+	require("dap").continue()
+end)
+vim.keymap.set("n", "<leader>di", function()
+	require("dap").step_into()
+end)
+vim.keymap.set("n", "<leader>do", function()
+	require("dap").step_over()
+end)
+vim.keymap.set("n", "<leader>dO", function()
+	require("dap").step_out()
+end)
+vim.keymap.set("n", "<leader>dr", function()
+	require("dap").repl.toggle()
+end)
+vim.keymap.set("n", "<leader>dl", function()
+	require("dap").run_last()
+end)
+vim.keymap.set("n", "<leader>dt", function()
+	require("dap-python").test_method()
+end)
+vim.keymap.set("n", "<leader>dT", function()
+	require("dap-python").test_class()
+end)
+
+-- Additional general settings
+vim.opt.expandtab = true -- Use spaces instead of tabs
+vim.opt.shiftwidth = 4 -- Size of an indent
+vim.opt.tabstop = 4 -- Number of spaces tabs count for
+vim.opt.smartindent = true -- Insert indents automatically
+vim.opt.wrap = false -- Don't wrap lines
+vim.opt.ignorecase = true -- Ignore case in search patterns
+vim.opt.smartcase = true -- Override ignorecase if search pattern contains upper case characters
+vim.opt.cursorline = true -- Highlight the current line
+vim.opt.mouse = "a" -- Enable mouse support
+vim.opt.updatetime = 250 -- Decrease update time
+vim.opt.signcolumn = "yes" -- Always show the signcolumn
+vim.opt.scrolloff = 8 -- Lines of context
+vim.opt.sidescrolloff = 8 -- Columns of context
+vim.opt.splitbelow = true -- Put new windows below current
+vim.opt.splitright = true -- Put new windows right of current
+vim.opt.timeoutlen = 300 -- Time in milliseconds to wait for a mapped sequence to complete
+
+-- Additional key mappings for better navigation
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+
+-- Better window resizing
+vim.keymap.set("n", "<C-Up>", ":resize -2<CR>")
+vim.keymap.set("n", "<C-Down>", ":resize +2<CR>")
+vim.keymap.set("n", "<C-Left>", ":vertical resize -2<CR>")
+vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>")
+
+-- Better indenting
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
+
+-- Move selected line / block of text in visual mode
+vim.keymap.set("x", "K", ":move '<-2<CR>gv-gv")
+vim.keymap.set("x", "J", ":move '>+1<CR>gv-gv")
+
+-- Better terminal navigation
+vim.keymap.set("t", "<C-h>", "<C-\\><C-N><C-w>h")
+vim.keymap.set("t", "<C-j>", "<C-\\><C-N><C-w>j")
+vim.keymap.set("t", "<C-k>", "<C-\\><C-N><C-w>k")
+vim.keymap.set("t", "<C-l>", "<C-\\><C-N><C-w>l")
+
+-- Additional LSP keybindings
+vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition)
+vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder)
+vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder)
+vim.keymap.set("n", "<leader>wl", function()
+	print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+end)
+
+-- Format on save for specific file types (in addition to existing ones)
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = { "*.json", "*.md", "*.yaml", "*.yml" },
+	callback = function()
+		vim.lsp.buf.format({ async = false })
+	end,
+})
+
+-- Auto commands for better user experience
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "qf", "help", "man", "lspinfo", "spectre_panel" },
+	callback = function()
+		vim.cmd([[
+            nnoremap <silent> <buffer> q :close<CR>
+            set nobuflisted
+        ]])
+	end,
+})
+
+-- Highlight on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
+
+-- Remember cursor position
+vim.api.nvim_create_autocmd("BufReadPost", {
+	callback = function()
+		local mark = vim.api.nvim_buf_get_mark(0, '"')
+		local lcount = vim.api.nvim_buf_line_count(0)
+		if mark[1] > 0 and mark[1] <= lcount then
+			pcall(vim.api.nvim_win_set_cursor, 0, mark)
+		end
+	end,
+})
+
+-- Auto reload files when changed on disk
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI" }, {
+	pattern = "*",
+	callback = function()
+		if vim.fn.mode() ~= "c" then
+			vim.cmd("checktime")
+		end
+	end,
+})
+
+-- Use relative line numbers in normal mode and absolute line numbers in insert mode
+vim.api.nvim_create_autocmd({ "InsertEnter" }, {
+	callback = function()
+		vim.opt.relativenumber = false
+	end,
+})
+
+vim.api.nvim_create_autocmd({ "InsertLeave" }, {
+	callback = function()
+		vim.opt.relativenumber = true
+	end,
+})
+
+-- Setup global status line
+vim.opt.laststatus = 3
+
+-- Initialize workspace diagnostics
+vim.diagnostic.config({
+	virtual_text = {
+		source = "always",
+		prefix = "●",
+	},
+	float = {
+		source = "always",
+	},
+})
